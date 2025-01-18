46da720f2b4c3f31b2f193d16706bb70ce60c9cf
Ngo Quang Bieu <nquangbieu@gmail.com>
2025-01-18 18:46:43 +0700

    Update

--- assets/script.js
+++ assets/script.js
@@ -5,33 +5,7 @@ tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-// Create an array of YouTube video IDs
-var videoIDs = [
-  'https://youtu.be/meGeGXVFsPw', 
-  'https://youtu.be/Jy6jJGIIUos', 
-  'https://youtu.be/7gMRMDWxTfo',
-  'https://youtu.be/vFVR_s2if7E',
-  'https://youtu.be/_CIFxSze_0w',
-  'https://youtu.be/ZaeYEX3qgRY',
-  'https://youtu.be/qqyzitqYvXA',
-  'https://youtu.be/3DOkxQ3HDXE',
-  'https://youtu.be/Gb2D-fQiHLI',
-  'https://youtu.be/tKVzm0SBYtQ',
-  'https://youtu.be/HHly9wFXYd8',
-  'https://youtu.be/kNYaN7fVAKo',
-  'https://youtu.be/WaXhd_U4rp0',
-  'https://youtu.be/2dpsU2XTI8k',
-  'https://youtu.be/shBbIhxh0iM',
-  'https://youtu.be/9BgNVW4T1eo',
-  'https://youtu.be/aAhW9gUUJCA',
-  'https://youtu.be/ulQoJ91jgYo',
-  'https://youtu.be/riJGGRviuuU',
-  'https://youtu.be/hO7jliJjM6s',
-  'https://youtu.be/7ATeQTV79A4',
-  'https://youtu.be/wrkCeXVdekM'
-]; // Replace with actual video IDs
 var currentVideoIndex = 0;
-var player;
 
 var vueApp = new Vue({
   el: "#app",
@@ -43,7 +17,6 @@ var vueApp = new Vue({
   },
   methods: {
     initYoutube() {
-      videoIDs = shuffleArray(videoIDs)
       var x = getYouTubeVideoID(videoIDs[currentVideoIndex])
       const _ = this;
       console.log("initYoutube");
@@ -82,15 +55,4 @@ function getYouTubeVideoID(url) {
   const regex = /(?:https?:\/\/(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/))([^"&?\/\s]{11})/;
   const match = url.match(regex);
   return match ? match[1] : null;
-}
-
-function shuffleArray(arr) {
-  for (let i = arr.length - 1; i > 0; i--) {
-      // Generate a random index
-      const j = Math.floor(Math.random() * (i + 1));
-      
-      // Swap elements at index i and j
-      [arr[i], arr[j]] = [arr[j], arr[i]];
-  }
-  return arr;
 }
